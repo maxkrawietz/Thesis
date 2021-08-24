f12f9d7e40ba79ef5e809e1e564e9f1244a78a0a
maxkrawietz <57331236+maxkrawietz@users.noreply.github.com>
2021-08-24 14:40:48 +0200

    heart disease dataset and plots

--- transformer.py
+++ transformer.py
@@ -16,6 +16,28 @@ class DeleteNAN(BaseEstimator, TransformerMixin):
         data_nan.dropna(how="any", inplace=True)
         return data_nan
 
+
+class ExchangeNan(BaseEstimator, TransformerMixin):
+    def __init__(self, nan_char, ex_char):
+        self.nan_char = nan_char
+        self.ex_char = ex_char
+
+    def set_nan_char(self, char):
+        self.nan_char = char
+
+    def set_ex_char(self, char):
+        self.ex_char = char
+    
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X, y=None):
+        data_nan= X.replace(self.nan_char,self.ex_char)
+        return data_nan
+
+
+
+
 class DataSelector(BaseEstimator, TransformerMixin):
     def __init__(self, value):
         self.value = value
